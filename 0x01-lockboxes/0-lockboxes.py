cac2cc939119824413b6005723a70aaab4b7a936
Mumin8 <alhassanmumin8@gmail.com>
2024-03-11 18:12:59 +0000

    function

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,11 +1,20 @@
+#!/usr/bin/python3
+
+
 def canUnlockAll(boxes):
+    '''
+    canUnlockAll:
+        this checks if all the boxes can be unlocked
+
+    return:
+        True if all the boxes can be unlocked
+        False if all the boxes cannot be unlocked
+    '''
     n = len(boxes)
     keys = set([0])
     opened = set()
-
     while keys:
         key = keys.pop()
         opened.add(key)
         keys.update(set(boxes[key]) - opened)
-
     return len(opened) == n
