98afeb5e420abb8e0384a3e6b6af639ff7592dbd
Mumin8 <alhassanmumin8@gmail.com>
2024-04-25 08:28:07 +0000

    rotate 2d matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+'''
+0-rotate_2d_matrix
+'''
+
 
 def rotate_2d_matrix(matrix):
+    '''
+    python scricpt to rotate a 2d matrix
+    '''
     return list(zip(*matrix[::-1]))
-    
