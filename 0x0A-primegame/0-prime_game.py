0d796e88c17cf61f4957943615098c3b4b670acd
Mumin8 <alhassanmumin8@gmail.com>
2024-05-16 12:39:01 +0000

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
+import copy
+
 '''0-prime_game'''
 
 
 def isWinner(x, nums):
     '''isWinner function'''
-
+    print(nums)
+    _nums = copy.deepcopy(nums)
     prime = [True for i in range(x + 1)]
     p = 2
     while (p * p <= x):
@@ -14,27 +17,25 @@ def isWinner(x, nums):
             for i in range(p * p, x + 1, p):
                 prime[i] = False
         p += 1
-
-    # Print all prime numbers
+    prime_ = []
     for p in range(2, x + 1):
         if prime[p]:
-            print(p)
+            prime_.append(p)
 
+    print(f'prime {prime_}')
     count = 1
-    for v in nums:
-        temp = count
-        idx = nums.index(v)
-        for val in prime:
-            if val == v:
-                count += 1
-                idx_ = prime.index(val)
-                prime.pop(idx_)
-                break
-        if temp != count:
-            nums.pop(idx)
+    for pn in prime_:
 
-    print(f'count {count}')
-    if count % 2 == 1:
+        l = len(_nums)
+        _nums = list(filter(lambda x: x % pn, _nums))
+        _nums = _nums
+        if l > len(_nums):
+            count += 1
+            prime_.pop(0)
+        else:
+            break
+
+    if count % 2 == 0:
         return "Maria"
     else:
         return "Ben"
